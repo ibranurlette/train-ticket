49617512a51f08853030571d8f07a064d590ed6b
Ibra Nurlette <sjsdev02@gmail.com>
2022-07-09 22:39:33 +0700

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "375px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "760px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1000px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1600px",
+      // => @media (min-width: 1024px) { ... }
+    },
   },
   plugins: [],
-}
+};
