8e42a3fa9327b8728ca3c40e0b7eb4716712f677
Ibra Nurlette <sjsdev02@gmail.com>
2022-07-16 23:38:12 +0700

    fetch register

--- src/client/_action/auth_register.js
+++ src/client/_action/auth_register.js
@@ -1,15 +1,14 @@
-import {AUTH} from '../config/constants';
-import {API} from '../config/api';
+import { AUTH } from "../config/constants";
+import { API } from "../config/api";
 
-export const register = data => {
+export const register = (data) => {
   return {
     type: AUTH,
     payload: async () => {
-      	const res = await API.post("/register", data);
-      	const {token} = res.data;
-      	 localStorage.setItem("token", token);
-      	 return res.data;
-    }
-
+      const res = await API.post("/register", data);
+      const { token } = res.data;
+      localStorage.setItem("token", token);
+      return res.data;
+    },
   };
 };
--- src/register/index.js
+++ src/register/index.js
@@ -1,10 +1,51 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { register } from "../client/_action/auth_register";
+
 const Register = () => {
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    gender: "",
+    phone: "",
+    addres: "",
+  });
+  const [error, setError] = useState("");
+
+  console.log("form", form);
+  const handleRegister = () => {
+    dispatch(register(form))
+      .then(async (res) => {
+        navigate("/", { replace: true });
+      })
+      .catch((err) => {
+        setError(err.response.data.message);
+      });
+  };
+
   return (
     <div className="mx-auto w-96 my-5">
       <form className="bg-white shadow-lg rounded px-8 pt-6 pb-8">
         <label className="block text-gray-700 text-lg font-bold mb-5">
           Halaman Register
         </label>
+        {error ? (
+          <div className="relative flex-auto border-b">
+            <div
+              className="bg-red-100 border-l-4 border-red-500 text-black-700 p-4"
+              role="alert"
+            >
+              <p>{error}</p>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
         <div className="">
           <div>
             <div className="mb-6">
@@ -18,6 +59,11 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="name"
                 type="text"
+                name="name"
+                value={form.name}
+                onChange={(e) => {
+                  setForm({ ...form, name: e.target.value });
+                }}
                 placeholder="Name"
               />
             </div>
@@ -32,6 +78,11 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="username"
                 type="text"
+                name="username"
+                value={form.username}
+                onChange={(e) => {
+                  setForm({ ...form, username: e.target.value });
+                }}
                 placeholder="Username"
               />
             </div>
@@ -46,6 +97,11 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="email"
                 type="email"
+                name="email"
+                value={form.email}
+                onChange={(e) => {
+                  setForm({ ...form, email: e.target.value });
+                }}
                 placeholder="Email"
               />
             </div>
@@ -60,6 +116,9 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 type="password"
+                name="password"
+                value={form.password}
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
                 placeholder="******************"
               />
             </div>
@@ -75,6 +134,9 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="gender"
                 type="text"
+                name="gender"
+                value={form.gender}
+                onChange={(e) => setForm({ ...form, gender: e.target.value })}
                 placeholder="gender"
               />
             </div>
@@ -89,20 +151,26 @@ const Register = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="phone"
                 type="number"
+                name="phone"
+                value={form.phone}
+                onChange={(e) => setForm({ ...form, phone: e.target.value })}
                 placeholder="phone"
               />
             </div>
             <div className="mb-6">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="alamat"
+                htmlFor="addres"
               >
                 Alamat
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="alamat"
+                id="addres"
                 type="text"
+                name="addres"
+                value={form.addres}
+                onChange={(e) => setForm({ ...form, addres: e.target.value })}
                 placeholder="Alamat"
               />
             </div>
@@ -110,6 +178,7 @@ const Register = () => {
             <button
               className="bg-gray-800 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
+              onClick={handleRegister}
             >
               Daftar
             </button>
